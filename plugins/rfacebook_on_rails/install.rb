86c6d5c158aedda3c6385a0ae6ae00359ef8094d
livelearncode <livelearncode@8ab7dc79-38fb-452c-aa03-a3fc60fd81f1>
2007-08-17 15:18:55 +0000

    Extra error-handling in plugin
    git-svn-id: http://rfacebook.rubyforge.org/svn@31 8ab7dc79-38fb-452c-aa03-a3fc60fd81f1

--- plugins/rfacebook_on_rails/install.rb
+++ plugins/rfacebook_on_rails/install.rb
@@ -1,1 +1,7 @@
-require "rfacebook_on_rails/plugin/install"
+require "rubygems"
+
+begin
+  require "rfacebook_on_rails/plugin/install"
+rescue Exception => e
+  raise StandardError, "There was a problem loading the RFacebook on Rails plugin.  You may have forgotten to install the RFacebook Gem."
+end
--- plugins/rfacebook_on_rails/uninstall.rb
+++ plugins/rfacebook_on_rails/uninstall.rb
@@ -1,1 +1,7 @@
-require "rfacebook_on_rails/plugin/uninstall"
+require "rubygems"
+
+begin
+  require "rfacebook_on_rails/plugin/uninstall"
+rescue Exception => e
+  raise StandardError, "There was a problem loading the RFacebook on Rails plugin.  You may have forgotten to install the RFacebook Gem."
+end
