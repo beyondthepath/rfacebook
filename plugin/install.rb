f37379addd968133395918192c4fa5d15d527c48
livelearncode <livelearncode@8ab7dc79-38fb-452c-aa03-a3fc60fd81f1>
2007-08-17 14:51:36 +0000

    Added a check in the plugin to ensure that you have the RFacebook gem installed.
    git-svn-id: http://rfacebook.rubyforge.org/svn@30 8ab7dc79-38fb-452c-aa03-a3fc60fd81f1

--- plugin/install.rb
+++ plugin/install.rb
@@ -1,2 +1,7 @@
 require "rubygems"
-require "rfacebook_on_rails/plugin/install"
+
+begin
+  require "rfacebook_on_rails/plugin/install"
+rescue Exception => e
+  raise StandardError, "There was a problem loading the RFacebook on Rails plugin.  You may have forgotten to install the RFacebook Gem."
+end
