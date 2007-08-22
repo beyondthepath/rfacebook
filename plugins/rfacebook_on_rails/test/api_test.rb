6a5a80e75d2174d28ee5e1f1b2754b5d0d124ee6
livelearncode <livelearncode@8ab7dc79-38fb-452c-aa03-a3fc60fd81f1>
2007-08-22 03:41:18 +0000

    Tests shouldn't be available in the plugin just yet.
    git-svn-id: http://rfacebook.rubyforge.org/svn@37 8ab7dc79-38fb-452c-aa03-a3fc60fd81f1

--- plugins/rfacebook_on_rails/test/api_test.rb
+++
@@ -1,9 +0,0 @@
-require "rubygems"
-
-begin
-  require File.dirname(__FILE__) + "/test_helper"
-  require "rfacebook_on_rails/tests/api_test"
-rescue Exception => e
-  puts "There was a problem loading the RFacebook on Rails plugin.  You may have forgotten to install the RFacebook Gem."
-  raise e
-end
--- plugins/rfacebook_on_rails/test/test_helper.rb
+++
@@ -1,2 +0,0 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
-require "action_controller/test_process"
